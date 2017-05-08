b73996ee48397bd4d73093ef757b8a6816b3e974
Yeyoxfunny <yeyox_114@hotmail.com>
2017-05-08 17:54:54 -0500

    Creacion de la vista de administracion de tipo de sugerencias

--- src/app/dashboard/manage-services/manage-services.component.ts
+++ src/app/dashboard/manage-services/manage-services.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+
+import { ISuggestionServiceCard } from '../../models/ISuggestionServiceCard';
+import { SUGGESTIONS_SERVICE_CARD } from '../../common/mock-suggestion-service-card';
 
 @Component({
-  selector: 'app-manage-services',
-  templateUrl: './manage-services.component.html',
-  styles: []
+	selector: 'app-manage-services',
+	templateUrl: './manage-services.component.html',
+	styles: []
 })
 export class ManageServicesComponent implements OnInit {
 
-  constructor() { }
+	TypeSuggestions : ISuggestionServiceCard[];
 
-  ngOnInit() {
-  }
+	hideElement : boolean = true;
 
+	constructor() {
+	}
+
+	ngOnInit() {
+		this.TypeSuggestions = SUGGESTIONS_SERVICE_CARD;
+	}
+
+	showNewItemForm(){
+		this.hideElement = false;
+	}
+
+	hideNewItemForm(){
+		this.hideElement = true;
+	}
 }
